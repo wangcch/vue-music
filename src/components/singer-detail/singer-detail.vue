1eeaf35d156d129e82b3390f98fa33887813a2ed
wangcch <ci713@qq.com>
2017-10-30 18:13:07 +0800

    update singer page. add click events.

---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,72 @@
+<template>
+  <transition name="slide">
+    <div class="singerdetail">
+      <img :src="singer.avatar" alt="">
+      <div v-html='singer.name'></div>
+    </div>
+  </transition>
+</template>
+
+<script type="text/ecmascript-6">
+  import { mapGetters } from 'vuex'
+  import { getSingerDetail } from 'api/singer'
+  import { ERR_OK } from 'api/config'
+  import { createSong } from 'common/js/song'
+  export default {
+    data() {
+      return {
+        songs: []
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'singer'
+      ])
+    },
+    created() {
+      this._getDetail()
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$roter.push('./singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSinger(res.data.list)
+            console.log(this.songs)
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .singerdetail
+    position: fixed
+    z-index: 100
+    top: 0
+    bottom: 0
+    left: 0
+    right: 0
+    background: $color-background
+
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
+</style>
